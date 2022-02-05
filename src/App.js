d09d1bbc54a21430e0fe052e915f0ab7fd04ce13
iamchiki <nikhil.soni599@gmail.com>
2022-02-05 22:41:33 +0530

    feat: configured router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
+import SideNavbar from './components/SideNavbar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import News from './components/News';
+import CryptoCurrencies from './components/CryptoCurrencies';
+import CryptoDetail from './components/CryptoDetail';
+
 function App() {
   return (
-    <h1 className='text-3xl font-bold underline text-blue-500'>Cyrpto app</h1>
+    <div className='flex bg-[#f0f2f5]'>
+      <div>
+        <SideNavbar></SideNavbar>
+      </div>
+
+      <div className='ml-[280px] flex-1 '>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
+          <Route
+            path='/cryptos'
+            element={<CryptoCurrencies coinLimit={100}></CryptoCurrencies>}
+          />
+          <Route path='/news' element={<News></News>} />
+          <Route
+            path='/cryptoDtl/:coinId'
+            element={<CryptoDetail></CryptoDetail>}
+          />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './store/redux-store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
