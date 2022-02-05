4e2586e7700699c25b84fe5afae3602c24428bed
iamchiki <nikhil.soni599@gmail.com>
2022-02-05 22:40:34 +0530

    chore: update config and package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        auto: 'repeat(auto-fit, minmax(230px, 1fr))',
+        'medium-auto': 'repeat(auto-fit, minmax(300px, 1fr))',
+      },
+    },
+    colors: {
+      'dark-blue': '#001529',
+      'light-indigo': '#6366f1',
+      'light-grey': '#d3d3d3',
+      white: '#fff',
+    },
   },
   plugins: [],
 };
+
+// auto: 'repeat(auto-fit, minmax(230px, 1fr))',
+// auto: 'repeat(auto-fit, 230px)',
