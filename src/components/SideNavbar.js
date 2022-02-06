449a7abd3100028b2a375b2f232afd881b5b5937
iamchiki <nikhil.soni599@gmail.com>
2022-02-06 19:22:01 +0530

    style: update navbar component

--- src/components/SideNavbar.js
+++ src/components/SideNavbar.js
@@ -1,34 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { cryptoIcon, homeIcon, newsIcon } from '../svg/icons';
+import cryptoHomeIcon from '../img/crypto-img.png';
 
 const SideNavbar = () => {
   return (
     <div className='w-64 h-screen bg-dark-blue fixed left-0'>
-      <div className='text-light-indigo'>
-        <h1>Crypto App</h1>
-      </div>
+      <Link to='/home'>
+        <div className='my-4 font-extrabold text-light-indigo text-2xl flex items-center justify-evenly'>
+          <img src={cryptoHomeIcon} alt='' />
+          <h1 className=''>Crypto App</h1>
+        </div>
+      </Link>
       <div>
-        <ul>
-          <li className='text-light-grey'>
+        <ul className='flex flex-col'>
+          <li className='text-light-grey p-2'>
             <Link to='/home'>
-              <div className='ml-4 flex gap-x-2 items-center'>
+              <div className='ml-4 flex gap-x-2 items-center hover:text-light-indigo'>
                 <span>{homeIcon}</span>
                 <h4>Home</h4>
               </div>
             </Link>
           </li>
-          <li className='text-light-grey'>
+          <li className='text-light-grey p-2'>
             <Link to='/cryptos'>
-              <div className='ml-4 flex gap-x-2 items-center'>
+              <div className='ml-4 flex gap-x-2 items-center hover:text-light-indigo'>
                 <span>{cryptoIcon}</span>
                 <h4>Cryptocurrencies</h4>
               </div>
             </Link>
           </li>
-          <li className='text-light-grey'>
+          <li className='text-light-grey p-2'>
             <Link to='/news'>
-              <div className='ml-4 flex gap-x-2 items-center'>
+              <div className='ml-4 flex gap-x-2 items-center hover:text-light-indigo'>
                 <span>{newsIcon}</span>
                 <h4>News</h4>
               </div>
